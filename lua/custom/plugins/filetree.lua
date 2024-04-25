16f0c0ab185d2892253ae3cdf00d7337c50bbddc
nielsdisveld <Niels.Disveld@finaps.nl>
2024-04-25 13:31:23 +0200

    Autoclose neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,7 +9,21 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup({
+      event_handlers = {
+
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+
+      }
+    })
   end,
 }
